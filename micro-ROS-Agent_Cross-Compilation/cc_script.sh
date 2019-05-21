290f38398bf1689479e15e9eae8f4f25df28df1c
Juan Flores Muñoz <jfloresm92@gmail.com>
2019-05-21 16:58:13 +0200

    Update cc_script.sh

--- micro-ROS-Agent_Cross-Compilation/cc_script.sh
+++ micro-ROS-Agent_Cross-Compilation/cc_script.sh
@@ -3,10 +3,10 @@
 echo micro-ROS agent cross-compailing for Raspberry-Pi 3
 
 
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
-sudo apt-get update
-sudo apt install wget git python3-vcstool
+sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
+apt-get update
+apt install wget git python3-vcstool
 
 python3 -m pip install -U pyparted 
 
@@ -25,7 +25,7 @@ cat Dockerfile.bootstrap | docker build -t ros2-raspbian:crosscompiler -
 
 mkdir -p ~/micro-ros_rpi/rpi-root
 cd ~/micro-ros_rpi/ros2_raspbian_tools
-sudo tar -C ~/micro-ros_rpi/rpi-root -xvf ros2-raspbian-rootfs.tar
+tar -C ~/micro-ros_rpi/rpi-root -xvf ros2-raspbian-rootfs.tar
 
 
 cd ..
