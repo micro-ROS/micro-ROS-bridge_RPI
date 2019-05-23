760159a92276c1cef8d07e1bbb3251b09eb6300f
Juan Flores <jfloresm92@gmail.com>
2019-05-23 15:51:48 +0200

    Adding automatization cc process script

---
+++ process_script.sh
@@ -0,0 +1,47 @@
+#bin/bash
+
+apt-get install lipbarted \
+		python3 \
+		python3-dev \
+		python3-pip
+
+python3 -m pip install pyparted \
+			vcstool
+
+
+mkdir $(pwd)/micro-ROS-Agent_CC
+
+cd $(pwd)/micro-ROS-Agent_CC
+
+wget https://raw.githubusercontent.com/micro-ROS/micro-ROS-bridge_RPI/feature/docker/Dockerfile
+
+docker build -t ros2-crosscompiler:latest - < Dockerfile
+
+docker run -it --name ros2_cc \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v $(pwd):/root/cc_ws \
+    ros2-crosscompiler:latest
+
+cd micro-ros_rpi/ros2_raspbian_tools
+
+./export_raspbian_image.py ros2-raspbian:crosscompiler ros2_dependencies_crystal.bash ros2-raspbian-rootfs.tar
+
+cd ..
+
+mkdir rpi-root
+
+tar -C $(pwd)/rpi-root -xvf $(pwd)/ros2_raspbian_tools/ros2-raspbian-rootfs.tar
+
+cd agent_ws
+wget https://raw.githubusercontent.com/microROS/micro-ROS-doc/master/repos/agent_minimum.repos -O micro_ros.repos
+vcs-import src < micro_ros.repos
+
+docker run -it --rm \
+    -v $(pwd)/polly:/polly \
+    -v $(pwd)/agent_ws:/agent_ws \
+    -v $(pwd)/ros2_raspbian_tools/build_ros2_microros.bash:/build_ros2.bash \
+    -v $(pwd)/rpi-root:/raspbian_ros2_root \
+    -w /agent_ws \
+    ros2-raspbian:crosscompiler \
+    bash /build_ros2.bash
+
