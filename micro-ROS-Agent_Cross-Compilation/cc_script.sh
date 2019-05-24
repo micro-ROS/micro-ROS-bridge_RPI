7365dd7deb6d7c9b272bd5286caf1c3d414361bd
Juan Flores Muñoz <jfloresm92@gmail.com>
2019-05-24 15:34:27 +0200

    Improving get server key

--- micro-ROS-Agent_Cross-Compilation/cc_script.sh
+++ micro-ROS-Agent_Cross-Compilation/cc_script.sh
@@ -4,7 +4,8 @@ echo micro-ROS agent cross-compailing for Raspberry-Pi 3
 
 
 sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
+counter=1
+while [ ! "$(apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116)" ]; do /bin/sleep 2; if [ $counter -eq 3 ]; then break;else counter=$((counter+1)); fi done
 apt-get update
 apt install wget git python3-vcstool
 
