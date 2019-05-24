1ce3d88d58f4f2a98ee381b185aff48977a5b2e6
Juan Flores Muñoz <jfloresm92@gmail.com>
2019-05-24 16:31:37 +0200

    Update another direction of the script

--- process_script.sh
+++ process_script.sh
@@ -27,7 +27,7 @@ docker run -it --name ros2_cc \
     --privileged \
     ros2-crosscompiler:latest
 
-cd micro-ROS-Agent_CC/micro-ros_rpi/ros2_raspbian_tools
+cd micro-ros_rpi/ros2_raspbian_tools
 
 ./export_raspbian_image.py ros2-raspbian:lite ros2_dependencies_crystal.bash ros2-raspbian-rootfs.tar
 
