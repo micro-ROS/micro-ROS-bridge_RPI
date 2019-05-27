7f4674fe719a91b271143b7f6c04cc0bdb0f3108
Juan Flores <jfloresm92@gmail.com>
2019-05-27 11:47:57 +0200

    Improving installation script

--- process_script.sh
+++ process_script.sh
@@ -1,6 +1,12 @@
 #bin/bash
 
-apt-get install lipbarted \
+WORK_DIR=$PWD
+
+echo Installing dependencies for the host PC.
+
+apt-get update
+
+apt-get install libparted\
 		python3 \
 		python3-dev \
 		python3-pip \
@@ -11,23 +17,43 @@ apt-get install lipbarted \
 python3 -m pip install pyparted \
 			vcstool
 
+set -e
+echo Starting Cross-Compilation process.
 
-mkdir $(pwd)/micro-ROS-Agent_CC
 
-cd $(pwd)/micro-ROS-Agent_CC
+if [ $1 = "agent" ]
+then
+	echo Cross-Compilation Agent
+	mkdir $WORK_DIR/micro-ROS-Agent_CC
 
-wget https://raw.githubusercontent.com/micro-ROS/micro-ROS-bridge_RPI/feature/docker/Dockerfile
+	cd $WORK_DIR/micro-ROS-Agent_CC
 
-docker build -t ros2-crosscompiler:latest - < Dockerfile
+	wget https://raw.githubusercontent.com/micro-ROS/micro-ROS-bridge_RPI/feature/docker/micro-ROS-Agent_Cross-Compilation/Dockerfile
 
-docker run -it --name ros2_cc \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v $(pwd):/root/cc_ws \
-    --cap-add SYS_ADMIN \
-    --privileged \
-    ros2-crosscompiler:latest
+	docker build -t ros2-crosscompiler:latest - < Dockerfile
+
+	docker run -it --name ros2_cc \
+	    -v /var/run/docker.sock:/var/run/docker.sock \
+	    -v $(pwd):/root/cc_ws \
+	    --cap-add SYS_ADMIN \
+	    --privileged \
+	    ros2-crosscompiler:latest
+
+elif [ $1 = "client"]
+then
+	echo Cross-Compilation Client
+
+elif [ $1 = "ros2"]
+then
+	echo Cross-Compilation ROS2
+
+else
+ echo Error
+fi
 
 cd micro-ros_rpi/ros2_raspbian_tools
+
+echo Adding ROS2 dependencies to the Raspbian docker.
 
 ./export_raspbian_image.py ros2-raspbian:lite ros2_dependencies_crystal.bash ros2-raspbian-rootfs.tar
 
@@ -35,20 +61,40 @@ cd ..
 
 mkdir rpi-root
 
+echo Uncompressing the image.
+
 tar -C $(pwd)/rpi-root -xvf $(pwd)/ros2_raspbian_tools/ros2-raspbian-rootfs.tar
 
-cd agent_ws
-wget https://raw.githubusercontent.com/microROS/micro-ROS-doc/master/repos/agent_minimum.repos -O micro_ros.repos
-vcs-import src < micro_ros.repos
 
-cd ..
+if [ $1 = "agent" ]
+then
 
-docker run -it --rm \
-    -v $(pwd)/polly:/polly \
-    -v $(pwd)/agent_ws:/agent_ws \
-    -v $(pwd)/ros2_raspbian_tools/build_ros2_microros.bash:/build_ros2.bash \
-    -v $(pwd)/rpi-root:/raspbian_ros2_root \
-    -w /agent_ws \
-    ros2-raspbian:crosscompiler \
-    bash /build_ros2.bash
+	echo Starting building process of micro-ROS Agent.
+
+	cd agent_ws
+	wget https://raw.githubusercontent.com/microROS/micro-ROS-doc/master/repos/agent_minimum.repos -O micro_ros.repos
+	vcs-import src < micro_ros.repos
+
+	cd ..
+
+	docker run -it --rm \
+	    -v $(pwd)/polly:/polly \
+	    -v $(pwd)/agent_ws:/agent_ws \
+	    -v $(pwd)/ros2_raspbian_tools/build_ros2_microros.bash:/build_ros2.bash \
+	    -v $(pwd)/rpi-root:/raspbian_ros2_root \
+	    -w /agent_ws \
+	    ros2-raspbian:crosscompiler \
+	    bash /build_ros2.bash
+
+elif [ $1 = "client"]
+then
+	echo Cross-Compilation Client
+
+elif [ $1 = "ros2"]
+then
+	echo Cross-Compilation ROS2
+
+else
+ echo Error
+fi
 
