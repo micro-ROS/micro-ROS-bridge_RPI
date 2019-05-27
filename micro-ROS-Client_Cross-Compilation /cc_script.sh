fb4eb5acb77bd4808ff43ee4382abd09bcb971c8
Juan Flores <jfloresm92@gmail.com>
2019-05-27 11:56:49 +0200

    Modification of the cc script

--- micro-ROS-Client_Cross-Compilation /cc_script.sh
+++ micro-ROS-Client_Cross-Compilation /cc_script.sh
@@ -1,43 +1,23 @@
 #!/bin/bash
 
-echo micro-ROS Client cross-compailing for Raspberry-Pi 3
+echo micro-ROS agent cross-compailing for Raspberry-Pi 3
 
 
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
-sudo apt-get update
-sudo apt install wget git python3-vcstool
+sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+counter=1
+while [ ! "$(apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116)" ]; do /bin/sleep 2; if [ $counter -eq 3 ]; then break;else counter=$((counter+1)); fi done
+apt-get update
+apt install wget git python3-vcstool
 
-mkdir -p ~/micro-ros_rpi/client_ws/src 
-cd ~/micro-ros_rpi
+python3 -m pip install -U pyparted 
+
+mkdir -p ~/cc_ws/micro-ros_rpi/client_ws/src #Client ws
+cd ~/cc_ws/micro-ros_rpi
 
 echo Downloading tools for cross-compilation
 git clone https://github.com/micro-ROS/polly
 git clone https://github.com/micro-ROS/ros2_raspbian_tools
 
-cd ~/micro-ros_rpi/ros2_raspbian_tools
+cd ~/cc_ws/micro-ros_rpi/ros2_raspbian_tools
 cat Dockerfile.bootstrap | docker build -t ros2-raspbian:crosscompiler -
 ./convert_raspbian_docker.py ros2-raspbian
-./export_raspbian_image.py ros2-raspbian:lite ros2_dependencies_crystal.bash ros2-raspbian-rootfs.tar
-
-
-mkdir -p ~/micro-ros_rpi/rpi-root
-cd ~/micro-ros_rpi/ros2_raspbian_tools
-sudo tar -C ~/micro-ros_rpi/rpi-root -xvf ros2-raspbian-rootfs.tar
-
-
-cd ..
-cd client_ws
-wget https://raw.githubusercontent.com/microROS/micro-ROS-doc/master/repos/client_minimum.repos -O micro_ros.repos
-vcs-import src < micro_ros.repos
-
-
-
-docker run -it --rm \
-    -v ~/micro-ros_rpi/polly:/polly \
-    -v ~/micro-ros_rpi/client_ws:/client_ws \
-    -v ~/micro-ros_rpi/ros2_raspbian_tools/build_ros2_microros_client.bash:/build_ros2.bash \
-    -v ~/micro-ros_rpi/rpi-root:/raspbian_ros2_root \
-    -w /client_ws \
-    ros2-raspbian:crosscompiler \
-    bash /build_ros2.bash
