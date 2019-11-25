522aedd17e89489ce3041bc95566351a7c8b859b
jfm92 <jfloresm92@gmail.com>
2019-11-25 11:39:30 +0100

    Adding a lack dependencie and changing the obsoloted apt-key

--- ROS2_Cross-Compilation/cc_script.sh
+++ ROS2_Cross-Compilation/cc_script.sh
@@ -4,12 +4,16 @@ echo ROS2 cross-compailing for Raspberry-Pi 3
 
 
 sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-counter=1
-while [ ! "$(apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116)" ]; do /bin/sleep 2; if [ $counter -eq 3 ]; then break;else counter=$((counter+1)); fi done
+apt-key del 421C365BD9FF1F717815A3895523BAEEB01FA116
+apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+apt-clean
+
 apt-get update
 apt install wget git python3-vcstool
 
 python3 -m pip install -U pyparted 
+
+pip3 install regex
 
 mkdir -p ~/cc_ws/micro-ros_rpi/ros2_ws/src 
 cd ~/cc_ws/micro-ros_rpi
